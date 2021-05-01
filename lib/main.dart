f9e8ce7fe7fba7a97db51aaa23b6d6a377d301f1
Ashraf <airriaislam@gmail.com>
2021-05-01 07:11:07 +0600

    Project cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'news.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -7,30 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hacker News',
+      theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          accentColor: Colors.blue,
+          brightness: Brightness.light),
+      home: News(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
---
+++ lib/news.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class News extends StatefulWidget{
+  @override
+  _NewsPageState createState() => _NewsPageState();
+
+}
+class _NewsPageState extends State<News>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+
+  }
+
+}
